82ae266141c590f99346126b300d3ec41dc4c33f
Ada Gottensträter <nunuu@gmx.net>
2022-04-11 17:52:37 +0200

    Relocate upper code to $5_0000, make room for save

--- build_gbrk.sh
+++ build_gbrk.sh
@@ -1,2 +1,2 @@
-flexspin -2 -E -H 262144 -O1,extrasmall --charset=shiftjis megayume_upper.spin2
+flexspin -2 -E -H 327680 -O1,extrasmall --charset=shiftjis megayume_upper.spin2
 flexspin -2 -l -gbrk megayume_lower.spin2
