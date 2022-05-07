9094ab7a1beb44b1d160cb72937d7c1c7a0d8101
Ada Gottensträter <nunuu@gmx.net>
2022-05-07 14:14:01 +0200

    Add inline-single to build config

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-flexspin -2 -E -H 327680 -O1,extrasmall --charset=shiftjis megayume_upper.spin2
+flexspin -2 -E -H 327680 -O1,extrasmall,inline-single --charset=shiftjis megayume_upper.spin2
 flexspin -2 -l megayume_lower.spin2
