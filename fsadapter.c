834d1468d60c57dd7bd8b38545c293ef99fd5bc8
Ada Gottensträter <nunuu@gmx.net>
2022-11-30 23:00:55 +0100

    Speed up directory scan

--- fsadapter.c
+++ fsadapter.c
@@ -3,18 +3,31 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-char *get_name(struct dirent *d) {
+char *get_name_for_dirent(struct dirent *d) {
     return d->d_name;
 }
 
-int get_type(const char *path) {
+
+int get_type_for_dirent(struct dirent *d) {
+    switch(d->d_type) {
+    case DT_REG:
+        return 2;
+    case DT_DIR:
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+int get_type_for_path(const char *path) {
     struct stat buf;
     if(!stat(path,&buf)) {
         if (S_ISDIR(buf.st_mode)) return 1;
         if (S_ISREG(buf.st_mode)) return 2;
     }
-    return 0;
 }
+
+
 
 int exists(const char *path) {
     struct stat buf;
