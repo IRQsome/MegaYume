fa162280571729f4ffd149ec108dacb9bb76f990
Ada Gottensträter <nunuu@gmx.net>
2023-10-19 20:42:56 +0200

    Update usbnew

--- build_comptest.sh
+++ build_comptest.sh
@@ -1,2 +1,2 @@
-../spin2cpp/build/flexspin -2 -E -H 327680 -O1,extrasmall,inline-single,experimental --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1 megayume_upper.spin2
+../spin2cpp/build/flexspin -2 -E -H 327680 -O1,extrasmall,inline-single,experimental,aggressive-mem --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1 megayume_upper.spin2
 ../spin2cpp/build/flexspin -2 -l megayume_lower.spin2
