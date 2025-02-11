98d4c4e95b852e01e803f1a60d09224775f03f8b
Ada Gottensträter <nunuu@gmx.net>
2025-02-11 17:31:29 +0100

    Fix build script due to change in default fcache size

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-flexspin -2 -E -H 327680 -O1,extrasmall,inline-single --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1  megayume_upper.spin2
+set -e # Enable error reporting
+rm megayume_lower.binary || true
+flexspin -2 -E -H 327680 -O1,extrasmall,inline-single --fcache=128 --charset=shiftjis -DFF_FS_TINY=1 -DFF_FS_NORTC=1  megayume_upper.spin2
 flexspin -2 -l megayume_lower.spin2
